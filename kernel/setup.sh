ff6acd886622e7efa94cd3129b7438f87f8da6fd
ggsasda <ggsasda@gmail.com>
2024-10-03 23:24:23 +0530

    Update setup.sh

--- kernel/setup.sh
+++ kernel/setup.sh
@@ -42,9 +42,7 @@ setup_kernelsu() {
     test -d "$GKI_ROOT/KernelSU" || git clone https://github.com/tiann/KernelSU && echo "[+] Repository cloned."
     cd "$GKI_ROOT/KernelSU"
     git stash && echo "[-] Stashed current changes."
-    if [ "$(git status | grep -Po 'v\d+(\.\d+)*' | head -n1)" ]; then
-        git checkout main && echo "[-] Switched to main branch."
-    fi
+        git checkout b766b98513b5a7eb33bc1c4a76b5702bf1288f07 && echo "[-] Switched to main branch."
     git pull && echo "[+] Repository updated."
     if [ -z "${1-}" ]; then
         git checkout "$(git describe --abbrev=0 --tags)" && echo "[-] Checked out latest tag."
